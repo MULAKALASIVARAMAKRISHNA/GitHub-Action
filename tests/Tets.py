26ae0ab1bbdf62f8b033c0a236c43a304d1cf802
siva <140964179+MULAKALASIVARAMAKRISHNA@users.noreply.github.com>
2025-03-10 15:56:57 +0530

    Update Tets.py

--- tests/Tets.py
+++ tests/Tets.py
@@ -1,7 +1,11 @@
-from src.mathoperations import add,sub
-def testing():
-    assert add(2,3) == 5
+from src.math_operations import add,sub
 
-    assert sub(3,4) == -1
-
-
+def test_add():
+    assert add(2,3)==5
+    assert add(-1,1)==0
+    
+def test_sub():
+    assert sub(5,3)==2
+    assert sub(4,3)==1
+    assert sub(3,3)==0
+    assert sub(2,3)==-1
